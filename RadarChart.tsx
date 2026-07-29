00be9f7eb8edba35b262d0113e97d9321a20ccf4
chrismiller2022-collab <chrismiller2022@gmail.com>
2026-07-29 10:26:15 -0400

    -

--- RadarChart.tsx
+++
@@ -1,8 +0,0 @@
-export default function ConfLink({ conf, onNavigateConference }: any) {
-  if (!onNavigateConference) return <>{conf}</>;
-  return (
-    <button className="conf-link" onClick={() => onNavigateConference(conf)}>
-      {conf}
-    </button>
-  );
-}
